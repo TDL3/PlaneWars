4196a9d39540e31cb2d2a20af03912d0ef704990
TDL3 <29535697+TDL3@users.noreply.github.com>
2020-08-19 21:41:44 +0800

    fornat code

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import definitions
 from sprites import Hero, Enemy, Bullet, Background
 
 
-class PlaneWars():
-    
+class PlaneWars:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(definitions.SCREEN_RECT.size)
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, 30)
         pygame.display.set_caption("Plane wars")
-        pygame.time.set_timer(definitions.ENEMY_SPAWN_EVENT, definitions.ENEMY_SPAWN_TIME)
+        pygame.time.set_timer(
+            definitions.ENEMY_SPAWN_EVENT, definitions.ENEMY_SPAWN_TIME
+        )
         self.__create_sprites()
-        
+
     def start_game(self):
         while True:
             self.clock.tick(definitions.GAME_FRAME)
@@ -22,17 +23,19 @@ class PlaneWars():
             self.__update_sprite()
             # render(text, antialias, color, background=None) -> Surface
             text = self.font.render("HP: " + str(self.hero.hp), True, (255, 55, 0))
-            self.screen.blit(text,(0,0))
-            
-            pygame.display.set_caption("Plane wars | FPS: " + str("{:.2f}".format(self.clock.get_fps())))
-            
+            self.screen.blit(text, (0, 0))
+
+            pygame.display.set_caption(
+                "Plane wars | FPS: " + str("{:.2f}".format(self.clock.get_fps()))
+            )
+
             pygame.display.update()
 
     def __create_sprites(self):
         bg1 = Background("./images/background.png")
-        bg2 = Background("./images/background.png",True)
+        bg2 = Background("./images/background.png", True)
         self.hero = Hero("./images/me1.png")
-        self.bg_group = pygame.sprite.Group(bg1,bg2)
+        self.bg_group = pygame.sprite.Group(bg1, bg2)
         self.hero_group = pygame.sprite.Group(self.hero)
         self.enemy_group = pygame.sprite.Group()
 
@@ -49,7 +52,9 @@ class PlaneWars():
 
         keys_pressed = pygame.key.get_pressed()
         if not keys_pressed[pygame.K_SPACE]:
-            pygame.time.set_timer(definitions.HERO_FIRE_EVENT,definitions.HERO_FIRE_INTERVAL)
+            pygame.time.set_timer(
+                definitions.HERO_FIRE_EVENT, definitions.HERO_FIRE_INTERVAL
+            )
         if keys_pressed[pygame.K_UP] or keys_pressed[pygame.K_w]:
             self.hero.speed = -4
         elif keys_pressed[pygame.K_DOWN] or keys_pressed[pygame.K_s]:
@@ -60,7 +65,7 @@ class PlaneWars():
             self.hero.speed_x = 4
         else:
             self.hero.speed_x = self.hero.speed = 0
-            
+
     def __check_collide(self):
         pygame.sprite.groupcollide(self.hero.bullet_group, self.enemy_group, True, True)
         enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
@@ -69,20 +74,25 @@ class PlaneWars():
             if self.hero.hp <= 0:
                 self.hero.kill()
                 self.__game_over()
-                
 
     def __update_sprite(self):
-        for group in [self.bg_group, self.hero_group, self.enemy_group,self.hero.bullet_group]:
-                group.update()
-                group.draw(self.screen)
-        
+        for group in [
+            self.bg_group,
+            self.hero_group,
+            self.enemy_group,
+            self.hero.bullet_group,
+        ]:
+            group.update()
+            group.draw(self.screen)
 
     def __game_over(self):
         pygame.quit()
         exit(0)
 
+
 def main():
     PlaneWars().start_game()
+
 
 if __name__ == "__main__":
     main()
--- sprites.py
+++ sprites.py
@@ -4,13 +4,14 @@ import definitions
 
 SCREEN_RECT = definitions.SCREEN_RECT
 
+
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image_dir, speed=1):
         super().__init__()
         self.image = pygame.image.load(image_dir)
         self.rect = self.image.get_rect()
         self.speed = speed
-    
+
     def update_image(self, img):
         self.image = img
 
@@ -22,17 +23,17 @@ class Background(GameSprite):
     def __init__(self, image_dir, is_second=False):
         super().__init__(image_dir)
         if is_second:
-            self.rect.y = - self.rect.height
+            self.rect.y = -self.rect.height
 
     def update(self):
         self.rect.y += self.speed
         if self.rect.y >= SCREEN_RECT.height:
-            self.rect.y = - self.rect.height
+            self.rect.y = -self.rect.height
 
 
 class Bullet(GameSprite):
     def __init__(self):
-        super().__init__("./images/bullet2.png",-3)
+        super().__init__("./images/bullet2.png", -3)
 
     def update(self):
         super().update()
@@ -41,20 +42,20 @@ class Bullet(GameSprite):
 
 
 class Hero(GameSprite):
-    def __init__(self,hero_dir):
-        super().__init__(hero_dir,0)
+    def __init__(self, hero_dir):
+        super().__init__(hero_dir, 0)
         self.change_time = 1
         self.changed_flag = False
         self.hp = 100
         self.speed_x = 0
-        self.rect.centerx = int(SCREEN_RECT.width/2)
-        self.rect.centery = SCREEN_RECT.height - 120 - int(self.rect.height/2)
+        self.rect.centerx = int(SCREEN_RECT.width / 2)
+        self.rect.centery = SCREEN_RECT.height - 120 - int(self.rect.height / 2)
         self.bullet_group = pygame.sprite.Group()
-        #pygame.time.set_timer(HERO_IMAGE_UPDATE_EVENT, HERO_IMAGE_INTERVAL)
+        # pygame.time.set_timer(HERO_IMAGE_UPDATE_EVENT, HERO_IMAGE_INTERVAL)
 
     def update(self):
         self.change_time += 1
-        #update image every 10 tick
+        # update image every 10 tick
         if self.change_time % 10 == 0:
             if self.changed_flag:
                 self.image = pygame.image.load("./images/me1.png")
@@ -62,7 +63,7 @@ class Hero(GameSprite):
             else:
                 self.image = pygame.image.load("./images/me2.png")
                 self.changed_flag = True
-        
+
         self.rect.x += self.speed_x
         self.rect.y += self.speed
         # Ensure plane stay inside the window
@@ -87,10 +88,12 @@ class Hero(GameSprite):
 
 
 class Enemy(GameSprite):
-    def __init__(self,enemy_dir):
+    def __init__(self, enemy_dir):
         super().__init__(enemy_dir)
-        self.speed = random.randint(definitions.ENEMY_SPEED_MIN,definitions.ENEMY_SPEED_MAX)
-        self.rect.x = random.randint(0,SCREEN_RECT.width - self.rect.width)
+        self.speed = random.randint(
+            definitions.ENEMY_SPEED_MIN, definitions.ENEMY_SPEED_MAX
+        )
+        self.rect.x = random.randint(0, SCREEN_RECT.width - self.rect.width)
         self.rect.y = -self.rect.height
         self.enemy_group = pygame.sprite.Group()
 
